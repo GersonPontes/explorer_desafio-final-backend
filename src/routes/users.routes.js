847ba6c8bb5a1a6a8379c559fd44ace48ea416e5
gerson <gersonpdeoliveira@gmail.com>
2024-03-04 09:43:10 -0300

    update

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,9 @@
 const { Router } = require("express");
 const UsersController = require("../controllers/UsersController");
-//const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
-//const UsersValidatedController = require("../controllers/UsersValidatedController");
 
 const usersRouter = Router();
 const usersController = new UsersController();
-//const usersValidatedController = new UsersValidatedController();
 
 usersRouter.post(("/"), usersController.create);
-//usersRouter.get(("/validated/:user_id"), usersValidatedController.index);
 
 module.exports = usersRouter;
